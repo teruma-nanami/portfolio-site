8b7aef7a5e68f0c3dece8964077ccf49507551ed
teruma-nanami <kinos.travel.fun@gmail.com>
2026-06-20 11:42:46 +0900

    feat: お問い合わせ通知メールのMailableを追加

---
+++ src/app/Mail/ContactNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public string $name,
+        public string $email,
+        public string $message,
+    ) {
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'お問い合わせがありました',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.contact',
+        );
+    }
+}
