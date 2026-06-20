a6c48eb63921e026d166a12891c4c912b0cbe8e8
teruma-nanami <kinos.travel.fun@gmail.com>
2026-06-20 11:49:46 +0900

    fix: レビュー指摘に基づきバリデーションメッセージを日本語化

---
+++ src/lang/ja/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'required' => ':attribute を入力してください。',
+
+    'string' => ':attribute は文字列で入力してください。',
+
+    'email' => ':attribute には、有効なメールアドレスを入力してください。',
+
+    'max' => [
+        'string' => ':attribute は :max 文字以下で入力してください。',
+    ],
+
+];
