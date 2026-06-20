e80d1662af755ee916494cd8b9e03f70a7beff45
teruma-nanami <kinos.travel.fun@gmail.com>
2026-06-20 11:43:55 +0900

    feat: お問い合わせページのルートを追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index')->name('profile');
 Route::view('/works', 'works')->name('works');
 Route::view('/process', 'process')->name('process');
+Route::view('/contact', 'contact')->name('contact');
+Route::post('/contact', [ContactController::class, 'store'])->middleware('throttle:6,1')->name('contact.store');
